ec0830b30f8091c9f5a7f7ca21d4a25a55ff6383
Malik <mlk.2plus@gmail.com>
2020-09-21 10:21:00 +0200

    Some inline CSS code in React.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,13 @@ import React from 'react';
 function malik(){
     return(
         <div>
-            <h1 style={{color: "red"}}>React tutorial</h1>
+            <h1 style={
+                {
+                    color: "red",
+                    fontSize: "100px",
+                    backgroundColor: "black"
+                }
+            }>React tutorial</h1>
         </div>
     );
 }
