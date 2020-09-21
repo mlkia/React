3cda0a85a5ea7f032f7797389c792b02b0a46dbe
Malik <mlk.2plus@gmail.com>
2020-09-21 10:14:13 +0200

    How is it work inline CSS code in React.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,17 +3,7 @@ import React from 'react';
 function malik(){
     return(
         <div>
-            <h1>React tutorial</h1>
-        </div>
-    );
-}
-
-
-
-function redman(){
-    return(
-        <div>
-            <h2>With Nour Humsi</h2>
+            <h1 style={{color: "red"}}>React tutorial</h1>
         </div>
     );
 }
