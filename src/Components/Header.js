4b4b6f176412dd68acdd0201fde009655dc745d0
Malik <mlk.2plus@gmail.com>
2020-09-21 11:21:40 +0200

    How is it work External CSS code in React.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 function malik(){
-    const style = {
-        color: "red",
-        fontSize: "100px",
-        backgroundColor: "black"
-    }
     return(
         <div>
-            <h1 style={style}>React tutorial</h1>
+            <h1>React tutorial</h1>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import Startsid from './Components/App.js'
+import './style.css'
 
 
 ReactDOM.render(<Startsid />, document.getElementById('root'));
