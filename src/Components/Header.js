b453e9d13f015355006c96e663a06a9c06934ed2
Malik <mlk.2plus@gmail.com>
2020-09-22 09:59:33 +0200

    How can change the html code to JS code in JSX.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,11 @@ function Header(){
     return(
         <div className="navbar bg-dark rounded">
             <h1 className="text-white">React tutorial.</h1>
+
+            <h2 className="text-white">5+6</h2>
+            /*The below code is JS, we write this between the blue braces*/
+            <h2 className="text-white">{5+6}</h2>
+
         </div>
     );
 }
