faa76004a8f9c61cab126cf525d9a8b93fdeb1e5
Malik <mlk.2plus@gmail.com>
2020-09-22 11:41:59 +0200

    If statement JS in React (First way)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function Header(){
+    let title= "";
+    const client= "ssc";
+
+    if (client === "Designer"){
+        title ="Design is my life."
+    }else{
+        title ="Programming is my life."
+    }
+
     return(
         <div className="navbar bg-dark rounded">
-            <h1 className="text-white">React tutorial.</h1>
-
-            <h2 className="text-white">5+6</h2>
-            /*The below code is JS, we write this between the blue braces*/
-            <h2 className="text-white">{5+6}</h2>
-
+            <h1 className="text-white">{title}</h1>
         </div>
     );
 }
